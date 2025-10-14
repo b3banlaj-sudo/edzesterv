33da9185c110ad709f597a375c839ca0af254066
b3banlaj-sudo <b3banlaj@vasvari.org>
2025-10-14 12:28:52 +0200

    2

--- edzesterv/Program.cs
+++ edzesterv/Program.cs
@@ -32,37 +32,7 @@ namespace edzesterv
 
 
 
-            //            2.A programnak a megadott bemenő adatokkal kell dolgoznia:
-            //            Edzés típusa(pl.Állóképesség → &quot; Futás / Kerékpározás & quot;, Izomtömeg → &quot; Súlyzós edzés&quot;,
-            //Fogyás → &quot; Intervall edzés&quot;).
-            //Edzés alaphossza(percben)
-            // Állóképesség: 45 perc
-            // Izomtömeg: 60 perc
-            // Fogyás: 30 perc
-
-            //Hibakezelés: Ha a beolvasott cél nem 1, 2 vagy 3, a program írjon ki hibaüzenetet, és állítsa
-            //az alaphosszt 0 - ra, a típust &quot; Érvénytelen cél&quot; -ra.
-
-            //3.A program számítsa ki a heti edzésterhelést!
-            //Kérdezze meg a felhasználótól, hány napot szeretne edzeni a héten(pl. 3, 4, 5 nap).
-            //Kérjen be minden edzésnapra egy erősségi szintet(1 - től 5 - ig, egész szám).Addig kell
-            //beolvasni, míg megfelelő értéket kapsz!
-            //Az új bemenő adatok alapján a program az alábbiakat számolja ki!
-            //Számítsa ki a napra vonatkozó edzésidőt:
-
-            //4.Számítás és Kimenet
-            //Végül program az adatok beolvasása után számítsa ki a teljes heti edzésidőt(az összes
-            //edzésnap időtartamának összege).
-            //Számítsa ki a becsült összes elégetett kalóriát a héten.
-
-            //Kalória szorzó a céltól függően:
-            // Állóképesség: 0.12
-            // Izomtömeg: 0.10
-            // Fogyás: 0.15
-
-            //Írja ki a felhasználó nevének, céljának, a heti teljes edzésidőnek és az elégetett kalóriának az
-
-            //összegzését a képernyőre.
+           
 
             while (cel > 0) {
                 Console.Write("Hány napot szeretne edzeni a héten: ");
