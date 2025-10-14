55c76b4b62683c03c59b76342469058d5a799571
b3banlaj-sudo <b3banlaj@vasvari.org>
2025-10-14 12:28:37 +0200

    2

--- edzesterv/Program.cs
+++ edzesterv/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,14 +24,73 @@ namespace edzesterv
             keresztnev = Console.ReadLine();
             Console.Write("(kilogrammban, valós szám 50 és 120 között) ");
             suly = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Kérem a célját (fogyás, izomtömeg növelés, állóképesség fejlesztés): ");
+            Console.Write("Kérem a célját (állóképesség fejlesztés, izomtömeg növelés, fogyás ): ");
             cel = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Köszönöm a válaszokat!");
 
 
+
+
+            //            2.A programnak a megadott bemenő adatokkal kell dolgoznia:
+            //            Edzés típusa(pl.Állóképesség → &quot; Futás / Kerékpározás & quot;, Izomtömeg → &quot; Súlyzós edzés&quot;,
+            //Fogyás → &quot; Intervall edzés&quot;).
+            //Edzés alaphossza(percben)
+            // Állóképesség: 45 perc
+            // Izomtömeg: 60 perc
+            // Fogyás: 30 perc
+
+            //Hibakezelés: Ha a beolvasott cél nem 1, 2 vagy 3, a program írjon ki hibaüzenetet, és állítsa
+            //az alaphosszt 0 - ra, a típust &quot; Érvénytelen cél&quot; -ra.
+
+            //3.A program számítsa ki a heti edzésterhelést!
+            //Kérdezze meg a felhasználótól, hány napot szeretne edzeni a héten(pl. 3, 4, 5 nap).
+            //Kérjen be minden edzésnapra egy erősségi szintet(1 - től 5 - ig, egész szám).Addig kell
+            //beolvasni, míg megfelelő értéket kapsz!
+            //Az új bemenő adatok alapján a program az alábbiakat számolja ki!
+            //Számítsa ki a napra vonatkozó edzésidőt:
+
+            //4.Számítás és Kimenet
+            //Végül program az adatok beolvasása után számítsa ki a teljes heti edzésidőt(az összes
+            //edzésnap időtartamának összege).
+            //Számítsa ki a becsült összes elégetett kalóriát a héten.
+
+            //Kalória szorzó a céltól függően:
+            // Állóképesség: 0.12
+            // Izomtömeg: 0.10
+            // Fogyás: 0.15
+
+            //Írja ki a felhasználó nevének, céljának, a heti teljes edzésidőnek és az elégetett kalóriának az
+
+            //összegzését a képernyőre.
+
+            while (cel > 0) {
+                Console.Write("Hány napot szeretne edzeni a héten: ");
+                int edzesnap = Convert.ToInt32(Console.ReadLine());
+                if (edzesnap >0 )
+                {
+                    for (int i = 1; i <= edzesnap; i++)
+                    {
+                        Console.Write($"Kérem az {i}. nap erősségi szintjét (1-5): ");
+                        int erosseg = Convert.ToInt32(Console.ReadLine());
+                        if (erosseg < 1 || erosseg > 5)
+                        {
+                            Console.WriteLine("Hibás adatot adott meg!");
+                            i--;
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás adatot adott meg!");
+                }
+            }
+
             if (cel == 1)
             {
+
+
 
             }
             else if (cel == 2)
@@ -49,6 +109,10 @@ namespace edzesterv
 
 
             Console.ReadKey();
+
+
+          
+
 
 
 
