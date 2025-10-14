30eecf5a065f5103cea922710b99be55359ac5d3
b3banlaj-sudo <b3banlaj@vasvari.org>
2025-10-14 12:13:46 +0200

    Add project files.

---
+++ edzesterv/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edzesterv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nev;
+            int suly;
+            int magassag;
+            double bmi;
+            double kerekites;
+
+            Console.Write("Kérem a nevét: ");
+            nev = Console.ReadLine();
+            Console.Write("Kérem a súlyát (kg): ");
+            suly = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kérem a magasságát (cm): ");
+            magassag = Convert.ToInt32(Console.ReadLine());
+            bmi = suly / Math.Pow((magassag / 100.0), 2);
+            kerekites = Math.Round(bmi, 2);
+            Console.WriteLine($"{nev}, az ön BMI értéke: {kerekites}");
+            
+        }
+    }
+}
